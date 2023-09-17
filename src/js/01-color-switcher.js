d960185c01bdce2bf2fc4243022c20725d0df1ec
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-09-17 22:12:34 +0200

    ready task 1 get random color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  const startButton = document.getElementById('data-start');
+  const stopButton = document.getElementById('data-stop');
+  const container = document.querySelector('.container');
+  let intervalId;
+  
+// Funkcja opoźnia pokazanie bordera po odświeżeniu strony - połaczona ze stylem transition w pliku css 
+  function showBorderWithDelay() {
+    container.style.borderColor = 'black';
+  }
+  
+  window.addEventListener('load', () => {
+    setTimeout(showBorderWithDelay, 1500);
+  });
+  
 
+// Kod do zadania generujacy losowy kolor
+  startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  
+    intervalId = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      container.style.backgroundColor = randomColor;
+      container.style.borderColor = 'transparent'; // Podczas zmiany kolorów border jest niewidoczny
+    }, 1000);
+  });
+  
+  stopButton.addEventListener('click', () => {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    container.style.borderColor = 'black'; // Po zatrzymaniu bordrer pojawia się
+    clearInterval(intervalId);
+  });
+  
